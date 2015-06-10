da81a51d1cfa50c536c6b1b6ef404c86c99b105b
Patryk Kaczmarek <patryk.k@samsung.com>
2015-06-10 13:37:01 +0200

    Crash fix
       * Screen reader was crashing on launch beacuse of NULL value passed to strncmp function
    
    Change-Id: I6723f342e3427c636b9aa42e32c06615101de9cd
    Signed-off-by: Patryk Kaczmarek <patryk.k@samsung.com>

--- src/screen_reader_vconf.c
+++ src/screen_reader_vconf.c
@@ -23,6 +23,9 @@ bool set_langauge(Service_Data *sd, const char *new_language, int new_voice)
    Eina_List *l;
    Voice_Info *vi;
 
+   if (!sd->language || !new_language)
+      return false;
+
    if(strncmp(sd->language, new_language, LAN_NAME - 1) == 0 && sd->voice_type == new_voice)
       {
          DEBUG("No need to change accessibility language: %s(%d) -> %s(%d)",
