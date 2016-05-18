29fe5cb92c4c6ec60267faf4d558447c3ef7f8fc
Shinwoo Kim <cinoo.kim@samsung.com>
2016-05-18 18:47:03 +0900

    direct reading from application synchronized with screen reader reading * added read command ID out parameter to ReadCommand method of org.tizen.DirectReading * added notification mechanism on reading stopped/cancelled/skipped * fixed segmentation fault during reading on application shows * added AtspiAccessible obj as parameter to tts_speak_customized * refactored existing calls to tts_speak * fixed initialization order for dbus_direct_reading_adapter * fixed lack of read cancel notification * fixed nested calls of tts_speak_do * fixes from review
    Change-Id: I6b0f620cf477807666f275a5f9558af3a4ae7218

--- include/screen_reader_tts.h
+++ include/screen_reader_tts.h
@@ -9,15 +9,44 @@ extern char* language;
 extern int voice;
 extern int speed;
 
-bool tts_init(void *data);
-void state_changed_cb(tts_h tts, tts_state_e previous, tts_state_e current, void* user_data);
-void tts_speak(char *text_to_speak, Eina_Bool flush_switch);
-void spi_stop(void *data);
+typedef struct {
+    char* text;
+    Eina_Bool discardable;
+    Eina_Bool want_discard_previous_reading;
+    Eina_Bool is_playing;
+    AtspiAccessible *obj;
+} Read_Command;
 
+bool tts_init(void *data);
+void tts_shutdown(void *data);
+void state_changed_cb(tts_h tts, tts_state_e previous, tts_state_e current, void* user_data);
+/**
+ * @brief Requests discardable, asynchronous (queued) reading of given text.
+ *
+ * @param text_to_speak a text that would be read by the Text To Speach (TTS) service using default TTS language
+ * @param want_discard_previous_reading determines if this reading wants the preceding reading to be discarded
+ * @return Read_Command* a pointer to the reading command suppporting this reading request
+ *
+ */
+Read_Command *tts_speak(char *text_to_speak, Eina_Bool want_discard_previous_reading);
+
+//TODO: add support for i18
+/**
+ * @brief Requests asynchronous (queued) reading of given text allowing to control,
+ * if it can be discarded by other reading requests
+ *
+ * @param text_to_speak a text that would be read by the Text To Speach (TTS) service using default TTS language
+ * @param want_discard_previous_reading determines if this reading wants the preceding reading to be discarded
+ * @param discardable determines if this reading request can be discarded by subsequent reading requests
+ * @param obj an accessible object associated with this reading request, which will be notified when the reading is stopped or canceled
+ * @return Read_Command* a pointer to the reading command suppporting this reading request
+ *
+ */
+Read_Command *tts_speak_customized(char *text_to_speak, Eina_Bool want_discard_previous_reading, Eina_Bool discardable, AtspiAccessible *obj);
+void spi_stop(void *data);
 void tts_stop_set(void);
 Eina_Bool tts_pause_get(void);
 Eina_Bool tts_pause_set(Eina_Bool pause_switch);
-
 void set_utterance_cb( void(*utter_cb)(void));
 
 #endif /* SCREEN_READER_TTS_H_ */
