1fe9cff2cdf9ea5573763c9ca8b55afc1e2d0192
Shilpa Singh <shilpa.singh@samsung.com>
2016-08-04 12:29:50 +0530

    [Screen-reader] Audio Access GUI 1.42 Signed-off-by: Shilpa Singh <shilpa.singh@samsung.com>
    Change-Id: I896de9c2bc7fca04e62ae8566e13ed57fde428e3

--- src/smart_notification.c
+++ src/smart_notification.c
@@ -18,11 +18,12 @@
 #include <Ecore_Evas.h>
 #include <Evas.h>
 #include <atspi/atspi.h>
-#include <tone_player.h>
 #include "logger.h"
 #include "screen_reader_tts.h"
 #include "screen_reader_haptic.h"
 #include "smart_notification.h"
+#include <wav_player.h>
+#include <sound_manager.h>
 
 #define RED  "\x1B[31m"
 #define RESET "\033[0m"
@@ -32,7 +33,28 @@ static Eina_Bool status = EINA_FALSE;
 static void _smart_notification_focus_chain_end(void);
 static void _smart_notification_realized_items(int start_idx, int end_idx);
 static void _smart_notification_highlight(void);
+static void _smart_notification_highlight_actionable(void);
+static void _smart_notification_action(void);
+static void _smart_notification_contextual_menu(void);
+static void _smart_notification_long_press(void);
+static void _smart_notification_state_change(void);
 
+static void
+_playback_completed_cb(int id, void *user_data)
+{
+	const char* path = (const char*)user_data;
+	DEBUG("WAV Player", "Completed! [id:%d, path:%s]", id, path);
+}
+
+static char *_get_sound_path(const char *file_path)
+{
+	char *dir;
+	dir = strdup(_TZ_SYS_RO_APP"/org.tizen.screen-reader/res/sounds");
+	char *path = g_strconcat(dir, "/", file_path, NULL);
+	free(dir);
+	DEBUG("path: (%s)", path);
+	return path;
+}
 /**
  * @brief Smart Notifications event handler
  *
@@ -55,6 +77,21 @@ void smart_notification(Notification_Type nt, int start_index, int end_index)
 		break;
 	case HIGHLIGHT_NOTIFICATION_EVENT:
 		_smart_notification_highlight();
+		break;
+	case HIGHLIGHT_NOTIFICATION_EVENT_ACTIONABLE:
+		_smart_notification_highlight_actionable();
+		break;
+	case ACTION_NOTIFICATION_EVENT:
+		_smart_notification_action();
+		break;
+	case CONTEXTUAL_MENU_NOTIFICATION_EVENT:
+		_smart_notification_contextual_menu();
+		break;
+	case LONG_PRESS_NOTIFICATION_EVENT:
+		_smart_notification_long_press();
+		break;
+	case WINDOW_STATE_CHANGE_NOTIFICATION_EVENT:
+		_smart_notification_state_change();
 		break;
 	default:
 		DEBUG("Gesture type %d not handled in switch", nt);
@@ -174,9 +211,13 @@ static void _smart_notification_focus_chain_end(void)
 		return;
 
 	DEBUG(RED "Smart notification - FOCUS CHAIN END" RESET);
+	int wav_player_id;
+	//sound to play when reached start/end not given hence using below.
+	gchar* wav_path = _get_sound_path("focus_general.ogg");
 
-	tone_player_stop(0);
-	tone_player_start(TONE_TYPE_SUP_CONFIRM, SOUND_TYPE_MEDIA, 200, NULL);
+	DEBUG("Smart notification - HIGHLIGHT" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
 }
 
 /**
@@ -187,11 +228,73 @@ static void _smart_notification_highlight(void)
 {
 	if (!status)
 		return;
+	int wav_player_id;
+	gchar* wav_path = _get_sound_path("focus_general.ogg");
 
-	DEBUG(RED "Smart notification - HIGHLIGHT" RESET);
+	DEBUG("Smart notification - HIGHLIGHT" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
+}
 
-	tone_player_stop(0);
-	tone_player_start(TONE_TYPE_PROP_BEEP, SOUND_TYPE_MEDIA, 200, NULL);
+static void _smart_notification_highlight_actionable(void)
+{
+	if (!status)
+		return;
+	int wav_player_id;
+
+	gchar* wav_path = _get_sound_path("focus_actionable.ogg");
+
+	DEBUG("Smart notification - HIGHLIGHT" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
+}
+
+static void _smart_notification_action(void)
+{
+	if (!status)
+		return;
+	int wav_player_id;
+	gchar* wav_path = _get_sound_path("clicked.ogg");
+
+	DEBUG("Smart notification - ACTION" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
+}
+
+static void _smart_notification_contextual_menu(void)
+{
+	if (!status)
+		return;
+	int wav_player_id;
+	gchar* wav_path = _get_sound_path("contextual_menu.ogg");
+
+	DEBUG("Smart notification - context menu" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
+}
+
+static void _smart_notification_long_press(void)
+{
+	if (!status)
+		return;
+	int wav_player_id;
+	gchar* wav_path = _get_sound_path("long_clicked.ogg");
+
+	DEBUG("Smart notification - long press" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
+}
+
+static void _smart_notification_state_change(void)
+{
+	if (!status)
+		return;
+	int wav_player_id;
+	gchar* wav_path = _get_sound_path("window_state_changed.ogg");
+
+	DEBUG("Smart notification - view change" RESET);
+	wav_player_start(wav_path, SOUND_TYPE_MEDIA, _playback_completed_cb, (void*)wav_path, &wav_player_id);
+	g_free(wav_path);
 }
 
 /**
